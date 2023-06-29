29441543cd5fd30ecca6bc326425e1c524cdffec
Martin Alegre <133788373+Martin-Alegre@users.noreply.github.com>
2023-06-28 22:47:50 -0300

    modificaciones (#9)

--- models/Manga.js
+++ models/Manga.js
@@ -2,12 +2,12 @@ import { Schema,model,Types } from "mongoose"
 
 let collection = 'mangas'
 let schema = new Schema({
-    author_id: { type:Types.ObjectId,ref: 'users',required:true },
-    company_id: { type:Types.ObjectId,ref: 'users' },
+    author_id: { type:Types.ObjectId,ref: 'authors',required:true },
+    company_id: { type:Types.ObjectId,ref: 'companies' },
     title: { type:String,required:true },
     cover_photo: { type:String,required:true },
     description: { type:String,required:true },
-    category_id: { type:Types.ObjectId,ref: 'users',required:true }
+    category_id: { type:Types.ObjectId,ref: 'categories',required:true }
 },{
     timestamps:true
 })
